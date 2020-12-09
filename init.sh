d2f2647c30fe63e7cf91a613d64966f3f4f950ab
uoooohub <uoooohub@protonmail.com>
2020-12-09 17:38:41 +0900

    update

--- init.sh
+++ init.sh
@@ -7,21 +7,23 @@ app_class_name=$4
 ## docker-compose.ymlと各サービスのDockerfileの構築
 sed -e "s/MY_WEB_SERVICE_NAME/${service_name}/g" tmp/docker-compose.yml > docker-compose.yml
 sed -e "s/MY_PROJECT_NAME/${site_name}/g" tmp/build/python/Dockerfile > python/Dockerfile
-#sed -i -e "s/MY_APP_NAME/${app_name}/g" tmp/build/python/Dockerfile
+sed -i -e "s/MY_APP_NAME/${app_name}/g" python/Dockerfile
 
 ## build
 docker-compose build $service_name .
 docker-compose run $service_name django-admin startproject $project_name .
 
 ## 各種設定の構築
-#sed -e "s/MY_APP_CLASS_NAME/${app_class_name}/g" tmp/build/python/app/models.py > python/app/models.py
-#sed -e "s/MY_APP_CLASS_NAME/${app_class_name}/g" tmp/build/python/app/admin.py > python/app/admin.py
-#sed -e "s/MY_APP_CLASS_NAME/${app_class_name}/g" tmp/build/python/app/views.py > python/app/views.py
-sed -e "s/MY_PROJECT_NAME/${site_name}/g" tmp/build/python/project/settings.py > python/project/settings.py
-sed -i -e "s/MY_APP_INSTALLED/${app_name}/g" python/project/settings.py
-#sed -e "s/MY_PROJECT_NAME/${site_name}/g" tmp/build/python/project/urls.py > python/project/urls.py
-#sed -e "s/MY_APP_NAME/${app_name}/g" tmp/build/python/project/urls.py > python/project/urls.py
+sed -e "s/MY_PROJECT_NAME/${site_name}/g" tmp/build/python/project/settings.py > src/$project_name/settings.py
+sed -i -e "s/MY_APP_INSTALLED/${app_name}/g" src/$project_name/settings.py
+sed -e "s/MY_PROJECT_NAME/${site_name}/g" tmp/build/python/project/urls.py > src/$project_name/urls.py
+sed -e "s/MY_APP_NAME/${app_name}/g" tmp/build/python/project/urls.py > src/$project_name/urls.py
 
+sed -e "s/MY_APP_CLASS_NAME/${app_class_name}/g" tmp/build/python/app/models.py > src/$app_name/models.py
+sed -e "s/MY_APP_CLASS_NAME/${app_class_name}/g" tmp/build/python/app/admin.py > src/$app_name/admin.py
+sed -e "s/MY_APP_CLASS_NAME/${app_class_name}/g" tmp/build/python/app/views.py > src/$app_name/views.py
+sed -e "s/MY_APP_NAME/${app_name}/g" tmp/build/python/project/urls.py > src/$app_name/urls.py
+cp -r tmp/build/python/app/templates > src/$app_name/templates
 
 docker-compose run $service_name ./manage.py makemigrations
 docker-compose run $service_name ./manage.py migrate
