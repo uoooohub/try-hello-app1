d3f7547f4e6f7b97db5a02307744184e0306a396
uoooohub <uoooohub@protonmail.com>
2020-12-09 19:34:47 +0900

    update

--- tmp/build/python/app/views.py
+++ tmp/build/python/app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    d = {
+        "year": datetime.now().year,
+        "month": datetime.now().month,
+        "day": datetime.now().day,
+        "hour": datetime.now().hour,
+        "message": "良いお天気ですね"
+    }
+    return render(request, "index.html", d)
